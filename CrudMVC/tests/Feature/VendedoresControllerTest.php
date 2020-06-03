74b4413a14f9fcb0aee9e3428d4802da5e7b2669
SergioFelzener <49769502+SergioFelzener@users.noreply.github.com>
2020-06-03 20:45:06 -0300

    PhPtests

---
+++ CrudMVC/tests/Feature/VendedoresControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Http\Controllers\VendedoresController;
+
+class VendedoresControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+
+
+
+     public function testCheckVendedor(){
+         $vendedores = new VendedoresController;
+
+         $this->assertTrue($vendedores->checkVendedor(1));
+     }
+
+}
